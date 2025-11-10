9a7251f13f23f9df9c32ee90a46a4e055351bdfe
thlhody <thlhody@gmail.com>
2025-11-10 10:15:06 +0200

    finished transfering legacy js to new es6 module

--- src/main/resources/static/js/legacy/session.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Create a new Bootstrap modal instance
-    var resumeModal = new bootstrap.Modal(document.getElementById('resumeConfirmationModal'));
-    // Show the modal
-    resumeModal.show();
-});
